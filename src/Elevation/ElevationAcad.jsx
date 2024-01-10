cbb7c4ef54eddf39b9b93548a190598dd79f2c8a
ayushsingh3007 <ayushsingh6394023007@gmail.com>
2024-01-10 04:41:50 -0800

    doing hard

--- src/Elevation/ElevationAcad.jsx
+++ src/Elevation/ElevationAcad.jsx
@@ -23,6 +23,249 @@ function ElevationAcad() {
 
 
       </section>
+       
+
+
+      <section className='forgraduate20222023'>
+         <div className='forgraduate20222023-left'>
+          <h1>For 2023, 2022 & 2021 graduates</h1>
+          <div className='batchstartdiv'>Batch Starting: 1st May 2023</div>
+         </div>
+         <div className='forgraduate20222023-right'>
+
+          <div  className='forgraduate20222023-right-item-1'>
+            <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/free_class.webp" alt="" />
+            <h4>Free Webinar</h4>
+            <p>17 April</p>
+          </div>
+
+             <div  className='linebetween'></div>
+
+          <div  className='forgraduate20222023-right-item-2'>
+          <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Start+Assesment.webp" alt="" />
+            <h4>1:1 Counselling session</h4>
+            <p>From 18th April</p>
+          </div>
+
+                 <div className='linebetween'></div>
+
+
+          <div  className='forgraduate20222023-right-item-3'> <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Batch+Starts.webp" alt="" />
+            <h4>Free Webinar</h4>
+            <p>17 April</p>
+            </div>
+
+
+
+         </div>
+      </section>
+
+
+       <section className='howtoreachdreamjob'>
+                   {/* <h2 style={{textAlign:'center'}}>Why PrepBytes Elevation Academy?</h2>  */}
+                  <div className='howtoreachdreamjob-left'>
+                    <div className="howtoreachdreamjob-left-item-1">
+                      <span className='span-1'>1</span><span>Attend Free Webinar</span>
+                      <hr/>
+                    </div>
+                    <div className="howtoreachdreamjob-left-item-2">
+                    <span className='span-2'>2</span><span>Attend Free Webinar</span>
+
+                    </div>
+                    <div className="howtoreachdreamjob-left-item-3">
+                    <span className='span-3'>3</span><span>Attend Free Webinar</span>
+
+                    </div>
+                    <div className="howtoreachdreamjob-left-item-4">
+                    <span className='span-4'>4</span><span>Attend Free Webinar</span>
+
+                    </div>
+                    <div className="howtoreachdreamjob-left-item-5">
+                    <span className='span-5'>5</span><span>Attend Free Webinar</span>
+
+                    </div>
+                  </div>
+
+
+
+
+
+
+
+
+                  <div className='howtoreachdreamjob-right'>
+
+                   <div className='howtoreachdreamjob-right-item-1'>
+                    <p>Apply, fill the form & wait till we review your application. You will be selected based on the eligibility criteria (2023, 2022 & 2021 passouts)</p>
+                    <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Application%2B%2BShortlisting.webp" alt="" />
+                   </div>
+                   <div  className='howtoreachdreamjob-right-item-1'>
+                   <p>Go through the free classes and learn the important concepts while understanding how the program unfolds.</p>
+                    <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/5%2BDays%2BFree%2BClass.webp" alt="" />
+                   </div>
+                   <div  className='howtoreachdreamjob-right-item-1'>
+                   <p>Embark on your journey of success once you receive the offer letter for successful enrolment.</p>
+                    <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Complete%2BEnrollment%2B.webp" alt="" />
+                   </div>
+                   <div  className='howtoreachdreamjob-right-item-1'>
+                   <p>You have now signed up for a learning-filled journey of 7 months that ends with you being placed in your dream company.</p>
+                    <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/4%2BMonths%2BPrograms.webp" alt="" />
+                   </div>
+                   <div  className='howtoreachdreamjob-right-item-1'>
+                   <p>The program comes with interview opportunities and a placement guarantee of minimum 5 LPA.</p>
+                    <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Interview%2BOpportunities%2B.webp" alt="" />
+                   </div>
+
+                  </div>
+       </section>
+
+
+        <section className='elevationlearningcard'>
+          <h2>Why PrepBytes Elevation Academy?</h2>
+             <div className='elevationlearningcard-container'>
+              <div className="elevationlearningcard-item">
+                <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Live+Learning.webp" alt="" />
+                <h4>Live Learning</h4>
+                <p>Master Mern Stack in Live Classes taken by Experts</p>
+              </div>
+              <div className="elevationlearningcard-item">
+              <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/1+week+of+Free+Classes.webp" alt="" />
+                <h4>Learn with Industry Experts</h4>
+                <p>Learn with leading tech mentors and gain access to real-time web development experience.</p>
+              </div>
+              <div className="elevationlearningcard-item">
+              <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/1+week+of+Free+Classes.webp" alt="" />
+                <h4>Learn by Doing</h4>
+                <p>Gain in depth experience by building real world projects</p>
+              </div>
+              <div className="elevationlearningcard-item">
+              <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Learn+by+Doing.webp" alt="" />
+                <h4>Resume Building</h4>
+                <p>Get a resume that companies would shortlist</p>
+              </div>
+              <div className="elevationlearningcard-item">
+              <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/1+week+of+Free+Classes.webp" alt="" />
+                <h4>Interview Opportunities</h4>
+                <p>Get developer interview opportunities after course completion</p>
+              </div>
+              <div className="elevationlearningcard-item">
+              <img src="https://s3.ap-south-1.amazonaws.com/www.prepbytes.com/images/elevation-academy/Images/Resume+Building.webp" alt="" />
+                <h4>Mock Interviews</h4>
+                <p>Interview practice with industry veterans with real-time feedbacks</p>
+              </div>
+             </div>
+        </section>
+
+         <section className='programsyllubus'>
+                <div className="programsyllubus-text">
+                 <h1>Program Syllabus</h1>
+                 <p>Learn from the best and be prepared to crack full stack developer jobs</p>
+                </div>
+         </section>
+
+
+       <section className='elevation-course-program-container'>
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+                   
+
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+
+              
+
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+                 
+
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+                
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+                  
+
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+                 
+
+                  <div className='elevation-course-program-container-item'>
+                    <div className='elevation-course-program-week'>
+                      <h4>1st Week - 4th Week</h4>
+                      </div><hr />
+                    <div className='elevation-course-program-developertype'>
+                      <div className='elevation-course-program-developertype-text'><span className='elevation-course-program-developertype-text-number'>1</span>  Frontend  Devlopment Basic </div>
+                      <div className='elevation-course-program-developertype-plus'>+</div>
+
+                    </div>
+                  </div>
+
+       </section>
+
+
+
+
+
+
     </>
   )
 }
